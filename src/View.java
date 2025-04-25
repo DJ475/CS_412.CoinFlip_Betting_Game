81eff584bf0ac5d79b124baa266e616f3ec6e1fc
miles witt <mileswitt0903@gmail.com>
2025-04-25 12:10:36 -0600

    Update View.java

--- src/View.java
+++ src/View.java
@@ -6,13 +6,19 @@ public class View {
     private JTabbedPane jtabs = new JTabbedPane();
     private ViewLeaderboard leaderBoardView = new ViewLeaderboard();
     private ViewGameplay gameplayView = new ViewGameplay();
+    private ViewCreate createView = new ViewCreate();
+    private ViewLogin loginView = new ViewLogin();
 
-    public View() {
+    public View()
+    {
+
     }
 
     public void InitializeUI() {
         this.jtabs.add("Play", this.gameplayView.MakeGameplay());
         this.jtabs.add("Leaderboard", this.leaderBoardView.MakeLeaderboard());
+        this.jtabs.add("Create", this.createView.MakeCreate());
+        this.jtabs.add("Login", this.loginView.MakeLogin());
         this.frameMain.add(this.jtabs);
         this.frameMain.setSize(600, 600);
         this.frameMain.setVisible(true);
@@ -25,4 +31,14 @@ public class View {
     public ViewGameplay getGameplayView() {
         return this.gameplayView;
     }
+
+    public ViewLogin getLoginView() {
+        return this.loginView;
+    }
+
+    public ViewCreate getCreateView() {
+        return this.createView;
+    }
+
 }
+
