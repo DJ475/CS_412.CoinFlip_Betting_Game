8c413fb1d4d8499fd7062befa49bf4b926389110
DJ475 <149053710+DJ475@users.noreply.github.com>
2025-04-28 15:26:57 -0600

    Create UserCreationClass.java
    Creating a dedicated class to handle user creation logic, separating it from the ClientHandler class. This follows the rule of separation of concerns.

---
+++ src/UserCreationClass.java
@@ -0,0 +1,23 @@
+import java.sql.SQLException;
+
+public class UserCreationClass {
+    private User u;
+
+    public void createUser(InputValidation inputValidationVariable, String[] valuesArray, UserModel userModelVar) throws SQLException {
+        System.out.println("Creating Account Now");
+        inputValidationVariable.setUsernameCreate(valuesArray[0]);
+        inputValidationVariable.setPasswordCreate(valuesArray[1]);
+        if(inputValidationVariable.ValidateCreateCreds())
+        {
+            u = new User(valuesArray[0],valuesArray[1], 0.00);
+
+//                                System.out.println("New user is: " + valuesArray[0]);
+//                                System.out.println("Password is: " + valuesArray[1]);
+            userModelVar.insertUserTable(u);
+        }
+        else
+        {
+            System.out.println("User input invalid");
+        }
+    }
+}
