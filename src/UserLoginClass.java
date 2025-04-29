7fefcfd18dcb8cfd154e6c3d0c40d6b5cd2e1035
DJ475 <149053710+DJ475@users.noreply.github.com>
2025-04-29 13:09:04 -0600

    Update UserLoginClass.java
    Added Logic to authenticate the hash of the provided password against the hash stored in the database for the account. If passed, then userobject is added to the logged-in-users treemap. If not passed, then the server gives an error to the user.

--- src/UserLoginClass.java
+++ src/UserLoginClass.java
@@ -1,7 +1,7 @@
 import java.sql.SQLException;
 
 public class UserLoginClass {
-    public void loginUser(InputValidation inputValidationVariable,String[] valuesArray, UserModel usermodelVar) throws SQLException {
+    public void loginUser(InputValidation inputValidationVariable, String[] valuesArray, UserModel usermodelVar, User u) throws SQLException {
         inputValidationVariable.setUsernameLogin(valuesArray[0]);
         inputValidationVariable.setPasswordLogin(valuesArray[1]);
         if(inputValidationVariable.ValidateLoginCreds())
@@ -9,7 +9,21 @@ public class UserLoginClass {
             System.out.println("User login is now: " + valuesArray[0]);
             System.out.println("Password is: " + valuesArray[1]);
             // Check if bcrypt comes back with true or false for correct and incorrect data entered
-            System.out.println("Authentication Result: " + usermodelVar.checkUserAuth(valuesArray[0],valuesArray[1]));
+            if(usermodelVar.checkUserAuth(valuesArray[0],valuesArray[1]))
+            {
+                System.out.println("Authentication Passed");
+                // authentication passed then add user to logged in user tree map
+                synchronized (Server.getUsersLoggedIn())
+                {
+                    Server.getUsersLoggedIn().put(u.getUsername(),u);
+                    System.out.println("Server Logged in users " + Server.getUsersLoggedIn());
+                }
+            }
+            else
+            {
+                System.out.println("Authentication Failed");
+            }
+//            System.out.println("Authentication Result: " + usermodelVar.checkUserAuth(valuesArray[0],valuesArray[1]));
         }
         else
         {
