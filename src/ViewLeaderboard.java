6232708c5b22528871baa6dfe83d6d8572b54ef6
DJ475 <149053710+DJ475@users.noreply.github.com>
2025-05-03 02:12:10 -0600

    Update ViewLeaderboard.java
    Added Jlist Functionality(DLM,SetListData())

--- src/ViewLeaderboard.java
+++ src/ViewLeaderboard.java
@@ -4,25 +4,40 @@ import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ViewLeaderboard {
     private JList topEarnerJlist;
     private DefaultListModel<String> dlm = new DefaultListModel();
+    private ArrayList<String> arrayList = new ArrayList<>();
 
     ViewLeaderboard() {
+        dlm = new DefaultListModel<>();
         this.topEarnerJlist = new JList(this.dlm);
+
     }
 
     public JPanel MakeLeaderboard() {
-        JPanel panelContainer = new JPanel();
-        this.dlm.addElement("Miles");
-        this.dlm.addElement("DJ");
-        panelContainer.setLayout(new BoxLayout(panelContainer, 1));
-        JLabel labelLeaderboard = new JLabel("TOP 3 Players");
-        this.topEarnerJlist.setFixedCellWidth(200);
-        panelContainer.add(labelLeaderboard, "Center");
-        panelContainer.add(this.topEarnerJlist, "Center");
-        return panelContainer;
+        try {
+            LeaderBoardModel leaderBoardModel = new LeaderBoardModel();
+            JPanel panelContainer = new JPanel();
+
+//        this.dlm.addElement("Miles");
+//        this.dlm.addElement("DJ");
+
+            panelContainer.setLayout(new BoxLayout(panelContainer, 1));
+            JLabel labelLeaderboard = new JLabel("TOP 3 Players");
+            this.topEarnerJlist.setFixedCellWidth(200);
+            panelContainer.add(labelLeaderboard, "Center");
+            panelContainer.add(this.topEarnerJlist, "Center");
+
+
+
+            return panelContainer;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public DefaultListModel<String> getDlm() {
