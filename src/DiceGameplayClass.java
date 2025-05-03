7b7a6cfa5dc1712bd8a0275a45028647f4f4341c
miles witt <mileswitt0903@gmail.com>
2025-05-03 15:08:50 -0600

    Create DiceGameplayClass.java

---
+++ src/DiceGameplayClass.java
@@ -0,0 +1,48 @@
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+public class DiceGameplayClass {
+    public void playUserGame(InputValidation inputValidationVariable, String[] valuesArray, PrintWriter prwBack, UserModel usermodelVar, String loggedInUser) throws SQLException {
+        if(Server.getUsersLoggedIn().containsKey(loggedInUser))
+        {
+//            System.out.println("Outcome is: " + new CoinFlip().CoinFlipLogic());
+            String betAmount = valuesArray[0];
+            String guessValue = valuesArray[1];
+            inputValidationVariable.setBetAmount(betAmount); // set bet amount given to server
+            if(inputValidationVariable.ValidateBetAmount() && guessValue != null) // check validity of input
+            {
+                System.out.println("Validation For bet Amount says: " + inputValidationVariable.ValidateBetAmount());
+                System.out.println("Value 1: " + valuesArray[0] +  " Value 2: " + valuesArray[1]);
+                System.out.println("Gameplay Mechanic Activated");
+                Diceroll dr = new Diceroll();
+                if(dr.DicerollLogic().equals(guessValue))
+                {
+                    prwBack.println("Guessed Correctly");
+                    synchronized (usermodelVar)
+                    {
+                        usermodelVar.updateUserTable(loggedInUser, Double.parseDouble(betAmount));
+                    }
+                }
+                else
+                {
+                    prwBack.println("Guess Incorrect");
+                    synchronized (usermodelVar)
+                    {
+                        double doubleValue = Double.parseDouble(betAmount);
+                        usermodelVar.updateUserTable(loggedInUser, -doubleValue);
+                    }
+                }
+
+                prwBack.println("Earnings: " + usermodelVar.selectUserEarnings(loggedInUser));
+            }
+            else
+            {
+                prwBack.println("Bet Values Invalid: (The Bet Amount Must Be a Positive Integer/Decimal, Must select Heads or Tails to continue");
+            }
+        }
+        else
+        {
+            prwBack.println("User Not Logged in");
+        }
+    }
+}
