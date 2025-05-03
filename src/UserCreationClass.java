d1436db38826b2d32781ec70a19f707ef944903c
DJ475 <149053710+DJ475@users.noreply.github.com>
2025-05-03 02:05:55 -0600

    Update UserCreationClass.java

--- src/UserCreationClass.java
+++ src/UserCreationClass.java
@@ -1,29 +1,26 @@
 import java.sql.SQLException;
 
-public class UserLoginClass {
-    public void loginUser(InputValidation inputValidationVariable, String[] valuesArray, UserModel usermodelVar, User u) throws SQLException {
-        inputValidationVariable.setUsernameLogin(valuesArray[0]);
-        inputValidationVariable.setPasswordLogin(valuesArray[1]);
-        if(inputValidationVariable.ValidateLoginCreds())
+public class UserCreationClass {
+    public void createUser(InputValidation inputValidationVariable, String[] valuesArray, UserModel userModelVar, User u) throws SQLException {
+        System.out.println("Creating Account Now");
+        inputValidationVariable.setUsernameCreate(valuesArray[0]);
+        inputValidationVariable.setPasswordCreate(valuesArray[1]);
+        if(inputValidationVariable.ValidateCreateCreds())
         {
-            System.out.println("User login is now: " + valuesArray[0]);
-            System.out.println("Password is: " + valuesArray[1]);
-            // Check if bcrypt comes back with true or false for correct and incorrect data entered
-            if(usermodelVar.checkUserAuth(valuesArray[0],valuesArray[1]))
+            u = new User(valuesArray[0],valuesArray[1], 0.00);
+
+//                                System.out.println("New user is: " + valuesArray[0]);
+//                                System.out.println("Password is: " + valuesArray[1]);
+
+            try
             {
-                System.out.println("Authentication Passed");
-                // authentication passed then add user to logged in user tree map
-                synchronized (Server.getUsersLoggedIn())
+                synchronized (Server.class)
                 {
-                    Server.getUsersLoggedIn().put(u.getUsername(),u);
-                    System.out.println("Server Logged in users " + Server.getUsersLoggedIn());
+                    userModelVar.insertUserTable(u);
                 }
+            } catch (SQLException e) {
+                System.out.println("ERROR When Creating New User: " + e.getMessage());
             }
-            else
-            {
-                System.out.println("Authentication Failed");
-            }
-//            System.out.println("Authentication Result: " + usermodelVar.checkUserAuth(valuesArray[0],valuesArray[1]));
         }
         else
         {
